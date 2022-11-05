4b029c40a64e65d783843fd00c0ff5c25dce5233
bakje <fambubberman@gmail.com>
2022-11-05 14:54:59 +0100

    added chat timestamps module cuz its cool ig 2

---
+++ src/main/java/me/bakje/bakjedev/bakjedev/module/Misc/chatTimestamps.java
@@ -0,0 +1,13 @@
+package me.bakje.bakjedev.bakjedev.module.Misc;
+
+import me.bakje.bakjedev.bakjedev.module.Mod;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+
+
+public class chatTimestamps extends Mod {
+    public chatTimestamps() {
+        super("Timestamps", "shows the time of a message", Category.MISC);
+    }
+
+    //mixin time
+}
