445c8b54277969e7f82e0627026ebf7c2ac3ee48
bakje <fambuberman@gmail.com>
2022-11-24 15:54:54 +0100

    fuck you epsacc bal sacc

--- src/main/java/me/bakje/bakjedev/bakjedev/module/Combat/PopIdentifier.java
+++ src/main/java/me/bakje/bakjedev/bakjedev/module/Combat/PopIdentifier.java
@@ -1,14 +1,14 @@
 package me.bakje.bakjedev.bakjedev.module.Combat;
 
-import me.bakje.bakjedev.bakjedev.event.events.SoundPlayEvent;
+import me.bakje.bakjedev.bakjedev.event.events.PacketEvent;
 import me.bakje.bakjedev.bakjedev.eventbus.BakjeSubscribe;
 import me.bakje.bakjedev.bakjedev.module.Mod;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.s2c.play.EntityStatusS2CPacket;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.math.Vec3d;
 
 
 public class PopIdentifier extends Mod {
@@ -16,15 +16,16 @@ public class PopIdentifier extends Mod {
         super("PopIdentifier", "identify player who pops a totem", Category.COMBAT, true);
     }
     @BakjeSubscribe
-    public void onSoundPlay(SoundPlayEvent.Normal event) {
-        String path = event.getInstance().getId().getPath();
-        if (path.equals("item.totem.use")) {
-            for (Entity e : mc.world.getEntities()) {
-                if (e instanceof PlayerEntity && e.getPos().equals(new Vec3d(event.getInstance().getX(),event.getInstance().getY(),event.getInstance().getZ()))) {
-                    MutableText prefixString = Text.literal("$ ").formatted(Formatting.YELLOW);
-                    mc.player.sendMessage(prefixString.append(Text.literal(e.getName().getString()+" popped").formatted(Formatting.GRAY)), false);
-                }
-            }
-        }
+    public void onReceivePacket(PacketEvent.Read event) {
+        if (!(event.getPacket() instanceof EntityStatusS2CPacket p )) return;
+
+        if (p.getStatus() != 35) return;
+
+        Entity e = p.getEntity(mc.world);
+
+        if (!(e instanceof PlayerEntity)) return;
+
+        MutableText prefixString = Text.literal("$ ").formatted(Formatting.YELLOW);
+        mc.player.sendMessage(prefixString.append(Text.literal(e.getName().getString()+" popped").formatted(Formatting.GRAY)), false);
     }
 }
